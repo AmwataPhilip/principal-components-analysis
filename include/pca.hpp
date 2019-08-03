669de2fcf7d4a20d4d8d90f4bd660e72195663ed
Philip Amwata <philip@philipamwata.net>
2019-08-03 13:24:56 +0200

    Basic methods and varribles added

--- include/pca.hpp
+++ include/pca.hpp
@@ -3,24 +3,38 @@
 
 #include <cstdio>
 #include <iostream>
-#include "Eigen/Dense"
+#include <utility>
+#include "Eigen/Core"
+#include "Eigen/LU"
+#include "Eigen/Cholesky"
+#include "Eigen/QR"
+#include "Eigen/SVD"
+#include "Eigen/Geometry"
+#include "Eigen/Eigenvalues"
+
+using namespace std;
+using namespace Eigen;
 
 class pca
 {
 private:
     /* data */
+    pair<float, float> eigenValues;
+    pair<float, float> eigenVectors;
+    Matrix2f covarianceMatrix;
+    float proportion1;
+    float proportion2;
+
 public:
     pca(/* args */);
     ~pca();
     void readFile();
+    void computeEigenValues();
+    void computeEigenVectors();
+    void computeCovarianceMatrix();
+    void computeTotalVariance();
+    void computeVarianceProportions();
+    void startPCS();
 };
-
-pca::pca(/* args */)
-{
-}
-
-pca::~pca()
-{
-}
 
 #endif // !PCA_HPP
--- src/pca.cpp
+++ src/pca.cpp
@@ -1,1 +1,17 @@
 #include "../include/pca.hpp"
+
+pca::pca(/* args */)
+{
+}
+
+pca::~pca()
+{
+}
+
+void readFile(){}
+void computeEigenValues(){}
+void computeEigenVectors(){}
+void computeCovarianceMatrix(){}
+void computeTotalVariance(){}
+void computeVarianceProportions(){}
+void startPCS(){}
